f7bdc434172367be6102f44cc16e94ff4b7dde59
Rostislav <rostislav@users.noreply.github.com>
2023-01-24 22:59:31 +0200

    hw: Fix printing of uninitialized values in emulator

--- src/hw/emu_runner.cpp
+++ src/hw/emu_runner.cpp
@@ -241,5 +241,8 @@ int emu_do_io(uint8_t *buf_in, uint16_t size_in, uint8_t *buf_out, uint16_t size
 
 int emu_do_io_i2c(uint8_t *buf, uint16_t size, uint32_t addr, int is_out)
 {
+	if (is_out) {
+		memset(buf, 0, size);
+	}
 	return 0;
 }

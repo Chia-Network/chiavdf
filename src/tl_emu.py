5aab75065f43b45e699c8c0e0c1ffb53a10daf22
Rostislav <rostislav@users.noreply.github.com>
2023-02-01 19:22:55 +0200

    tl_emu.py: Print discriminant of the challenge sent to VDF client

--- src/tl_emu.py
+++ src/tl_emu.py
@@ -68,6 +68,7 @@ async def init_conn(reader, writer, idx):
     msg = b"N%03d%s%c%s" % (len(d), d, len(INIT_FORM), INIT_FORM)
     await send_msg(writer, msg)
     print("Sent initial value for VDF %d, cnt %d" % (idx, cnts[idx]))
+    print(" d = %s" % (d.decode(),))
 
     ok = decode_resp(await reader.read(2))
     if ok != "OK":

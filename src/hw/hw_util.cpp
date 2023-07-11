b17a7120abcacd43c547051dadc44c83f749f8f1
Rostislav <rostislav@users.noreply.github.com>
2023-07-11 17:02:22 +0300

    hw: Add milliseconds to timestamps in logs

--- src/hw/hw_util.cpp
+++ src/hw/hw_util.cpp
@@ -12,8 +12,8 @@ void vdf_do_log(const char *msg, ...)
 
     clock_gettime(CLOCK_REALTIME, &ts);
     localtime_r(&ts.tv_sec, &cal);
-    strftime(time_str, sizeof(time_str), "%FT%T ", &cal);
-    fputs(time_str, stderr);
+    strftime(time_str, sizeof(time_str), "%FT%T", &cal);
+    fprintf(stderr, "%s.%03ld ", time_str, ts.tv_nsec / 1000000);
 
     va_start(ap, msg);
     vfprintf(stderr, msg, ap);

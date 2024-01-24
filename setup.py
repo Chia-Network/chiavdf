3cd0004bf4d8caee5813ef13e565b1c93cc30cdf
Earle Lowe <30607889+emlowe@users.noreply.github.com>
2024-01-23 16:29:26 -0800

    Add python 3.12 (#160)
    * Add python 3.12
    
    * Add python 3.12
    
    * Update cibuildwheel to 2.16.2
    
    * Try using delvewheel on windows
    
    * Update pyproject.toml
    
    * Update pyproject.toml
    
    * Update pyproject.toml
    
    * Update pyproject.toml
    
    * Update pyproject.toml
    
    * Remove distutils
    
    * Tweak project settings
    
    * don't set SETUPTOOLS_USE_DISTUTILS=stdlib
    
    * Don't do 3.12 on riscv for now
    
    * Remove need for looseversion
    
    * remove unused re package
    
    * lint fix

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 import os
 import platform
-import re
 import shutil
 import subprocess
 import sys
-from distutils.command.install import install
-from distutils.version import LooseVersion
 
 from setuptools import Command, Extension, setup, errors
 from setuptools.command.build import build
 from setuptools.command.build_ext import build_ext
+from setuptools.command.install import install
 
 BUILD_HOOKS = []
 INSTALL_HOOKS = []
@@ -93,20 +91,13 @@ if BUILD_VDF_BENCH:
 class CMakeBuild(build_ext):
     def run(self):
         try:
-            out = subprocess.check_output(["cmake", "--version"])
+            subprocess.check_output(["cmake", "--version"])
         except OSError:
             raise RuntimeError(
                 "CMake must be installed to build"
                 + " the following extensions: "
                 + ", ".join(e.name for e in self.extensions)
             )
-
-        if platform.system() == "Windows":
-            cmake_version = LooseVersion(
-                re.search(r"version\s*([\d.]+)", out.decode()).group(1)
-            )
-            if cmake_version < "3.1.0":
-                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
 
         for ext in self.extensions:
             self.build_extension(ext)

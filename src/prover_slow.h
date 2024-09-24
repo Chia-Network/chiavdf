1b3d9ff3978ec2fe2d702248f24a03fd2a1ee13a
William Blanke <wjb98672@gmail.com>
2024-09-24 10:07:26 -0700

    log

--- src/prover_slow.h
+++ src/prover_slow.h
@@ -117,8 +117,8 @@ std::vector<uint8_t> ProveSlow(integer& D, form& x, uint64_t num_iterations, std
         if ((num_iterations&0xffff)==0) {
             struct stat buffer;
             char sz[200];
-            sprintf(sz,"num_iterations %d shutdown_file_path.c_str() %s",num_iterations,shutdown_file_path.c_str())
-            logit(sz)
+            sprintf(sz,"num_iterations %llu shutdown_file_path.c_str() %s",num_iterations,shutdown_file_path.c_str())
+            logit(sz);
             if ((shutdown_file_path!="") && (stat (shutdown_file_path.c_str(), &buffer) != 0))
                 logit("aborting");     
                 return {};

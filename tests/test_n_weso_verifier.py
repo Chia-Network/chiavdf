c1a25d8850d08b4442589dd077398519a81ee217
William Blanke <wjb98672@gmail.com>
2024-10-12 17:38:00 -0700

    cleanup

--- tests/test_n_weso_verifier.py
+++ tests/test_n_weso_verifier.py
@@ -1,3 +1,5 @@
+import secrets
+
 from chiavdf import (
     create_discriminant,
     prove,
@@ -9,7 +11,6 @@ from chiavdf import (
 
 
 def prove_n_weso(discriminant_challenge, x, discriminant_size, form_size, iters, witness, wrong_segm):
-    wjb = 0
     iters_chunk = iters // (witness + 1)
     partials = []
     discriminant = create_discriminant(discriminant_challenge, discriminant_size)
@@ -39,9 +40,6 @@ def prove_n_weso(discriminant_challenge, x, discriminant_size, form_size, iters,
     for x, y, proof in reversed(partials):
         b_hex = get_b_from_n_wesolowski(discriminant, x, y + proof, iters_chunk, 0)
         b = int(b_hex, 16)
-        wjb = wjb + 1
-        print(f"{wjb} verify_wesolowski discriminant {discriminant}\n  x {x.hex()}")
-        print(f"  y {y.hex()}\n  proof {proof.hex()}\n  iters_chunk {iters_chunk}")
         assert verify_wesolowski(discriminant, x, y, proof, iters_chunk)
         is_valid, y_from_compression = verify_n_wesolowski_with_b(
             discriminant,
@@ -65,7 +63,7 @@ def prove_n_weso(discriminant_challenge, x, discriminant_size, form_size, iters,
 
 
 def test_prove_n_weso_and_verify():
-    discriminant_challenge = b'\xa6\xc4%X\x17O\xb1\xee\xdcd'
+    discriminant_challenge = secrets.token_bytes(10)
     discriminant_size = 512
     discriminant = create_discriminant(discriminant_challenge, discriminant_size)
     print(f"discriminant_challenge {discriminant_challenge.hex()} discriminant {discriminant}")

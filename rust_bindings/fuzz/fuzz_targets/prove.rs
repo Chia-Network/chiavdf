ff0d74e94b120d570247703b983a5fc630394971
arvidn <arvid@libtorrent.org>
2025-12-18 11:30:12 +0100

    simplify fuzzers by using integration with Arbitrary

--- rust_bindings/fuzz/fuzz_targets/prove.rs
+++ rust_bindings/fuzz/fuzz_targets/prove.rs
@@ -1,12 +1,9 @@
 #![no_main]
 
 use chiavdf::prove;
-use libfuzzer_sys::{arbitrary::Unstructured, fuzz_target};
+use libfuzzer_sys::fuzz_target;
 
-fuzz_target!(|data: &[u8]| {
-    let mut unstructured = Unstructured::new(data);
-    let genesis_challenge: [u8; 32] = unstructured.arbitrary().unwrap();
-    let element: [u8; 100] = unstructured.arbitrary().unwrap();
-    let iters: u8 = unstructured.arbitrary().unwrap();
+fuzz_target!(|data: ([u8; 32], [u8; 100], u16)| {
+    let (genesis_challenge, element, iters) = data;
     prove(&genesis_challenge, &element, 1024, iters as u64);
 });
